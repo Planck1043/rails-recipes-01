590eb677414c27a86640c614dbfb2ecdf4de63e6
Planck1043 <zhao1036713209@gmail.com>
2017-09-21 23:21:39 +0800

    all things

---
+++ app/helpers/admin/events_tickets_helper.rb
@@ -0,0 +1,2 @@
+module Admin::EventsTicketsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "events#index"
-    resources :events
-    resources :users
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+    end
+    resources :users do
+      resource :profile, :controller => "user_profiles"
+    end
   end
 
   root "events#index"
